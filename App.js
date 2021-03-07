dbe85f549dd43286e3d73a4e32fa87f7954e868d
MoonK <ansrud45@gmail.com>
2021-03-07 16:32:06 +0900

    update README

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('moon');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}</Text>
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,6 @@
+# React Native Tutorial
+
+## ✔ Views, Text & Styles
+
+- View is a container that supports layout with flexbox, style, some touch handling, and accessibility controls. (div라고 생각하면 될 것 같다.)
+- Text is a react component for displaying text.
